f745499f1f35c5d896dfc9796b7691f9ee24c0fd
Nemo.luong <viethaihn1994@gmail.com>
2018-02-02 17:22:48 +0700

    add Product function on sidebar

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,6 +1,6 @@
 <a href="{!! URL::action('Admin\CategoryController@index') !!}">Category </a>
 <br>
-{{--<a href="{!! URL::action('CategoryController@index') !!}">Category </a>--}}
+<a href="{!! URL::action('Admin\ProductController@index') !!}">Product </a>
 <br>
 {{--<a href="{!! URL::action('ProductController@index') !!}">Product </a>--}}
 <br>
